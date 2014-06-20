411235dc9db91608e730a75d0f28e487aa11b481
Arjuna <arjun.kashyap@yahoo.co.in>
2014-06-20 19:48:05 +0530

    Commit No. 33 - Hard link error

--- php/prasthanatraya_list.php
+++ php/prasthanatraya_list.php
@@ -50,10 +50,10 @@ elseif($_SESSION['valid'] != 1)
 		<div class="map">
 			<img src="images/AkhandaBharata.png" alt="Akhanda Bharata" usemap="#amnayapeetham"/>
 			<map name="amnayapeetham">
-				<area shape="rect" coords="15,220,150,270"  alt="Tattvamasi" title="छान्दोग्योपनिषत् (६-८-७)" target="_blank" href="format.php?bhashya=Chandogya&hval=%E0%A4%A4%E0%A4%A4%E0%A5%8D%E0%A4%A4%E0%A5%8D%E0%A4%B5%E0%A4%AE%E0%A4%B8%E0%A4%BF#Ch_C06_S08_V07">
-				<area shape="rect" coords="115,349,212,411" alt="Aham Brahmasmi" title="बृहदारण्यकोपनिषत् (१-४-१०)" target="_blank" href="format.php?bhashya=Brha&hval=%E0%A4%85%E0%A4%B9%E0%A4%82%20%E0%A4%AC%E0%A5%8D%E0%A4%B0%E0%A4%B9%E0%A5%8D%E0%A4%AE%E0%A4%BE%E0%A4%B8%E0%A5%8D%E0%A4%AE%E0%A5%80%E0%A4%A4%E0%A4%BF#BR_C01_S04_V10">
-				<area shape="rect" coords="320,201,419,256" alt="Prajnanam Brahma" title="ऐतरेयोपनिषत् ( ३-१-३)" target="_blank" href="format.php?bhashya=Aitareya&hval=%E0%A4%AA%E0%A5%8D%E0%A4%B0%E0%A4%9C%E0%A5%8D%E0%A4%9E%E0%A4%BE%E0%A4%A8%E0%A4%82%20%E0%A4%AC%E0%A5%8D%E0%A4%B0%E0%A4%B9%E0%A5%8D%E0%A4%AE#AI_C03_S01_V03">
-				<area shape="rect" coords="276,77,387,133" alt="Ayamatma Brahma" title="माण्डूक्योपनिषत् (२)" target="_blank" href="format.php?bhashya=Mandukya&hval=%E0%A4%B9%E0%A5%8D%E0%A4%AF%E0%A5%87%E0%A4%A4%E0%A4%A6%E0%A5%8D%E0%A4%AC%E0%A5%8D%E0%A4%B0%E0%A4%B9%E0%A5%8D%E0%A4%AE%E0%A4%BE%E0%A4%AF%E0%A4%AE%E0%A4%BE%E0%A4%A4%E0%A5%8D%E0%A4%AE%E0%A4%BE%20%E0%A4%AC%E0%A5%8D%E0%A4%B0%E0%A4%B9%E0%A5%8D%E0%A4%AE#MK_V02">
+				<area shape="rect" coords="15,220,150,270"  alt="Tattvamasi" title="छान्दोग्योपनिषत् (६-८-७)" target="_blank" href="format.php?bhashya=Chandogya&page=06&hval=%E0%A4%A4%E0%A4%A4%E0%A5%8D%E0%A4%A4%E0%A5%8D%E0%A4%B5%E0%A4%AE%E0%A4%B8%E0%A4%BF#Ch_C06_S08_V07">
+				<area shape="rect" coords="115,349,212,411" alt="Aham Brahmasmi" title="बृहदारण्यकोपनिषत् (१-४-१०)" target="_blank" href="format.php?bhashya=Brha&page=01&hval=%E0%A4%85%E0%A4%B9%E0%A4%82%20%E0%A4%AC%E0%A5%8D%E0%A4%B0%E0%A4%B9%E0%A5%8D%E0%A4%AE%E0%A4%BE%E0%A4%B8%E0%A5%8D%E0%A4%AE%E0%A5%80%E0%A4%A4%E0%A4%BF#BR_C01_S04_V10">
+				<area shape="rect" coords="320,201,419,256" alt="Prajnanam Brahma" title="ऐतरेयोपनिषत् ( ३-१-३)" target="_blank" href="format.php?bhashya=Aitareya&page=03&hval=%E0%A4%AA%E0%A5%8D%E0%A4%B0%E0%A4%9C%E0%A5%8D%E0%A4%9E%E0%A4%BE%E0%A4%A8%E0%A4%82%20%E0%A4%AC%E0%A5%8D%E0%A4%B0%E0%A4%B9%E0%A5%8D%E0%A4%AE#AI_C03_S01_V03">
+				<area shape="rect" coords="276,77,387,133" alt="Ayamatma Brahma" title="माण्डूक्योपनिषत् (२)" target="_blank" href="format.php?bhashya=Mandukya&page=01&hval=%E0%A4%B9%E0%A5%8D%E0%A4%AF%E0%A5%87%E0%A4%A4%E0%A4%A6%E0%A5%8D%E0%A4%AC%E0%A5%8D%E0%A4%B0%E0%A4%B9%E0%A5%8D%E0%A4%AE%E0%A4%BE%E0%A4%AF%E0%A4%AE%E0%A4%BE%E0%A4%A4%E0%A5%8D%E0%A4%AE%E0%A4%BE%20%E0%A4%AC%E0%A5%8D%E0%A4%B0%E0%A4%B9%E0%A5%8D%E0%A4%AE#MK_V02">
 			</map>
 		</div>
 		<div class="maincontent fsan sml clr" style="padding-bottom:20px;">

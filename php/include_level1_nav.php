83dba48dc082bd796dccede0e3980596f481b929
Arjuna <arjun.kashyap@yahoo.co.in>
2014-09-05 14:13:38 +0530

    Commit No. 54 - Adhyaya/pada jump to location using Ajax

--- php/include_level1_nav.php
+++ php/include_level1_nav.php
@@ -17,7 +17,8 @@ foreach ($xml->div->div->div as $verse)
 		$vstring = (string) $versetext->__toString();
 		$vstring = preg_split("/ /", $vstring);
 		$vstring = $vstring[0] . " " . $vstring[1] . " " . $vstring[2];
-		echo "<li class=\"sml\"><a class=\"further sml\" href=\"#".$verse['id']."\">" . convert_devanagari($i) . ". " . $vstring . "</a></li>";
+        
+        echo "<li class=\"sml\"><a class=\"further sml\" href=\"javascript:void(0);\" onclick=\"scrollTo( '#" . $verse['id'] . "', '01', '" . $_POST['bhashya'] . "', '" . $_POST['hval'] . "', '" . $_POST['level'] . "' )\">" . convert_devanagari($i) . ". " . $vstring . "</a></li>";
 		$i++;
 	}
 }
--- php/include_level4_nav.php
+++ php/include_level4_nav.php
@@ -23,7 +23,7 @@ foreach ($xml->div->div->div as $chapter)
         $sName = (string) $section->div[0];
         $sName = preg_replace("/॥/", "", $sName);
 	
-    	echo "<li><a href=\"format.php?bhashya=" . $_POST['bid'] . "&page=" . $page_num . "#".$section['id']."\">· " . $sName . "</a></li>";
+        echo "<li><a href=\"javascript:void(0);\" onclick=\"scrollTo( '#" . $section['id'] . "', '" . $page_num . "', '" . $_POST['bhashya'] . "', '" . $_POST['hval'] . "', '" . $_POST['level'] . "' )\">· " . $sName . "</a></li>";
 	}
 	echo "</ul></li>";
 }

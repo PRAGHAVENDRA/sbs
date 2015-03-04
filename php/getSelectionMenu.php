fd6debb3eed4a2cfd5fecc7c8ae33907aea7c54e
arjunkashyap <arjun.kashyap@yahoo.co.in>
2015-03-04 15:43:05 +0530

    Commit No. 121 - Selection menu and vyajhya data fetched through php - underway

--- php/getSelectionMenu.php
+++ php/getSelectionMenu.php
@@ -1,6 +1,34 @@
-    <ul>
-        <li><a class="showvyakhya" data-vyakhya="BM" data-parent="sel_BS_C01_S01_I01_W01">भामति</a></li>
-        <li><a class="showvyakhya" data-vyakhya="NN" data-parent="sel_BS_C01_S01_I01_W01">न्यायनिर्णय</a></li>
-        <li><a class="showvyakhya" data-vyakhya="PP" data-parent="sel_BS_C01_S01_I01_W01">पञ्चपादिक</a></li>
-        <li><a class="showvyakhya" data-vyakhya="RP" data-parent="sel_BS_C01_S01_I01_W01">रत्नप्रभ</a></li>
-    </ul>
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+</head>
+<body>
+
+<?php
+
+$id = $_GET['id'];
+
+$vyakhyaCode = array("1"=>"BM","2"=>"NN","3"=>"PP","4"=>"RP");
+$vyakhyaName = array("BM"=>"भामति","NN"=>"न्यायनिर्णय","PP"=>"पञ्चपादिक","RP"=>"रत्नप्रभ");
+
+foreach ($vyakhyaCode as $vk) {
+	if (file_exists('vyakhya/' . $vk . '.xml')) {
+    	$xml = simplexml_load_file('vyakhya/' . $vk . '.xml');
+	}
+	else {
+	    exit( 'Failed to open' );
+	}
+}
+
+?>
+
+<ul>
+    <li><a class="showvyakhya" data-vyakhya="BM" data-parent="sel_<?=$id?>">भामति</a></li>
+    <li><a class="showvyakhya" data-vyakhya="NN" data-parent="sel_<?=$id?>">न्यायनिर्णय</a></li>
+    <li><a class="showvyakhya" data-vyakhya="PP" data-parent="sel_<?=$id?>">पञ्चपादिक</a></li>
+    <li><a class="showvyakhya" data-vyakhya="RP" data-parent="sel_<?=$id?>">रत्नप्रभ</a></li>
+</ul>
+</body>
+</html>

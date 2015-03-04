5e50c1e38c56e889fd651b29f96c966f190158a8
arjunkashyap <arjun.kashyap@yahoo.co.in>
2015-03-04 18:52:52 +0530

    Commit No. 123 - Selection menu generated

--- php/getSelectionMenu.php
+++ php/getSelectionMenu.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-</head>
-<body>
-
 <?php
 
 $id = $_GET['id'];
@@ -13,22 +5,15 @@ $id = $_GET['id'];
 $vyakhyaCode = array("1"=>"BM","2"=>"NN","3"=>"PP","4"=>"RP");
 $vyakhyaName = array("BM"=>"भामति","NN"=>"न्यायनिर्णय","PP"=>"पञ्चपादिक","RP"=>"रत्नप्रभ");
 
+echo '<ul>';
 foreach ($vyakhyaCode as $vk) {
 	if (file_exists('vyakhya/' . $vk . '.xml')) {
     	$xml = simplexml_load_file('vyakhya/' . $vk . '.xml');
-	}
-	else {
-	    exit( 'Failed to open' );
-	}
+
+    	foreach ($xml->xpath('div[@id="' . $id . '_' . $vk . '"]') as $div) {
+ 			echo '<li><a class="showvyakhya" data-vyakhya="' . $vk . '" data-parent="sel_' . $id . '">' . $vyakhyaName{$vk} . '</a></li>';
+ 		}
+   	}
 }
-
+echo '</ul>';
 ?>
-
-<ul>
-    <li><a class="showvyakhya" data-vyakhya="BM" data-parent="sel_<?=$id?>">भामति</a></li>
-    <li><a class="showvyakhya" data-vyakhya="NN" data-parent="sel_<?=$id?>">न्यायनिर्णय</a></li>
-    <li><a class="showvyakhya" data-vyakhya="PP" data-parent="sel_<?=$id?>">पञ्चपादिक</a></li>
-    <li><a class="showvyakhya" data-vyakhya="RP" data-parent="sel_<?=$id?>">रत्नप्रभ</a></li>
-</ul>
-</body>
-</html>
